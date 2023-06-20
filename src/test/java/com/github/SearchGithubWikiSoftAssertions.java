a9adf090957a8867fa4b526bb79bfe8f5521ab12
Ivan Safronov <kiraaaakka@gmail.com>
2023-06-20 18:25:45 +0300

    fix

--- src/test/java/com/github/SearchGithubWikiSoftAssertions.java
+++ src/test/java/com/github/SearchGithubWikiSoftAssertions.java
@@ -1,19 +1,22 @@
 package com.github;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class searchGithubWikiSoftAssertions {
+public class SearchGithubWikiSoftAssertions {
+
+    @BeforeAll
+    static void conf() {
+        Configuration.browserSize = "1920x1080";
+    }
 
     @Test
     void shouldBeJunit () {
-        Configuration.browserSize = "1920x1080";
         open("https://github.com/selenide/selenide");
         $("#wiki-tab").click();
         $("[data-filterable-for=wiki-pages-filter]")
@@ -31,8 +34,6 @@ public class searchGithubWikiSoftAssertions {
                 "    $(\"#second\").should(visible).click();\n" +
                 "  }\n" +
                 "}"));
-
-        sleep(5000);
     }
 
 }
